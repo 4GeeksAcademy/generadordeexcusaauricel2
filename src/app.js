a7854d23542a5ae7dfe7bf3dd74592194a29c88b
Auricel <148658628+Auricel@users.noreply.github.com>
2024-07-08 10:30:26 +0000

    generador excusa auricel

--- src/app.js
+++ src/app.js
@@ -5,7 +5,29 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+function generadorDeExcusas() {
+  const quienes = ["Mi Perro", "Mi Abuela", "Mis tortugas", "Mis pajaros"];
+  const acciones = ["corrio", "rompio", "comio", "salto"];
+  const quecosas = ["Mi Proyecto", "La bicicleta", "Mis llaves"];
+  const momentos = [
+    "Antes de Clases",
+    "Justo a Tiempo",
+    "Cuando termine",
+    "Durante mi Almuerzo",
+    "Mientras estaba Durmiendo"
+  ];
+
+  const quien = quienes[Math.floor(Math.random() * quienes.length)];
+  const accion = acciones[Math.floor(Math.random() * quienes.length)];
+  const que = quecosas[Math.floor(Math.random() * quienes.length)];
+  const cuando = momentos[Math.floor(Math.random() * quienes.length)];
+
+  return `${quien} ${accion} ${que} ${cuando}.`;
+}
+
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  document.getElementById(
+    "excuse"
+  ).innerHTML = `<p id = "excuse">${generadorDeExcusas()}</p>`;
 };
